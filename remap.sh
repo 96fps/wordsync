82326d23aa7a6ac301b1ec1e7c13ba30db0b8fd0
Péter Finta <finta96peter@gmail.com>
2019-08-13 00:07:24 -0400

    minor fixes

--- remap.sh
+++ remap.sh
@@ -2,7 +2,6 @@
 mydir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
 
 
-#!/bin/bash
 if test "$#" -lt 2; then
     echo "Illegal number of parameters"
     echo "expected a .wav file and .csv mapping"
@@ -11,21 +10,19 @@ else
 output="out.wav"
 if test "$#" = 3; then
     echo "outputing to file: $3"
-output=$3
+  output="$3"
 
 fi
-# word=0
+word=0
 dur=0
 pe1=0
 pe2=0
-printf "generating clips"
-
 while IFS=, read -r col1 s1 e1 s2 e2
 do
-  # ((word=word+1))
+  ((word=word+1))
 
 
-  # n="$(printf "%04d\n" $word |dos2unix)"
+  n="$(printf "%04d\n" $word )"
 
 if (( $(echo "$pe1 > $s1" | bc -l) ))
 then
@@ -52,14 +49,13 @@ fi
   tempo="$(echo  "$d1 / $d2"  | bc -l)"
 
 pad=$(printf '%0.1s' " "{1..60})
+  # printf "%02d %s" $word $col1 
 
-  # printf " %s" $col1 
-  # printf '%*.*s' 0 $((8 - ${#col1} )) "$pad"
-  # printf "+%.2f :%.2f×\n" $pad2 $tempo
+#   printf " %s" $col1 
+# printf '%*.*s' 0 $((8 - ${#col1} )) "$pad"
+#  printf " +%.2f :%.2f×\n" $pad2 $tempo
 
-  printf "." 
-
- 
+printf "."
   sox $1 -p trim $s1 $de1 \
   | sox - -p tempo -s $tempo \
   | sox - -p trim 0 $d2 \
@@ -73,9 +69,10 @@ pad=$(printf '%0.1s' " "{1..60})
   
 
 done < $2
+ # "$mydir/mapi.csv"
+
 printf "\n"
 echo "Merging clips"
 sox "$mydir/part_*.wav" $output
-echo "Completed! see file $output"
-
+echo "Completed! see $output"
 fi
